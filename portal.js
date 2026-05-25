c4e5104d202d6b602df363c6bfaa8241e0001ae7
Sky-Aurora-Industries <rahmadagilu@gmail.com>
2026-05-26 00:48:02 +0800

    Delete portal.js

--- portal.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.buy-now').addEventListener('click', function() {
-    alert("Mengalihkan ke gateway pembayaran aman Sky-Aurora...");
-    window.location.href = "https://buy.stripe.com/test_dummy_link"; 
-});
